f388d2e333166b24300ae1e5ca83d86b32e136ed
Sean Geary <sgeary@flexera.com>
2021-05-11 11:05:58 -0400

    Fix artifact name

--- report_artifacts.py
+++ report_artifacts.py
@@ -286,7 +286,7 @@ def generate_html_report(reportData):
     # Grab the current date/time for report date stamp
     now = datetime.now().strftime("%B %d, %Y at %H:%M:%S")
     
-    htmlFile = projectName + "-" + reportName.replace(" ", "_") + ".html"
+    htmlFile = projectName.replace(" - ", "-").replace(" ", "_") + "-" + reportName.replace(" ", "_") + ".html"
     logger.debug("htmlFile: %s" %htmlFile)
 
     #---------------------------------------------------------------------------------------------------
